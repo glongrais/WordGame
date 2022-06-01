44a9004b2d6953d75807e7a018acd6212ddc3a7b
Guillaume Longrais <guillaume.longrais@orange.fr>
2022-06-01 17:20:33 +0200

    Created LetterBox view file

---
+++ WordGame/LetterBox.swift
@@ -0,0 +1,20 @@
+//
+//  LetterBox.swift
+//  WordGame
+//
+//  Created by Guillaume Longrais on 2022-06-01.
+//
+
+import SwiftUI
+
+struct LetterBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LetterBox_Previews: PreviewProvider {
+    static var previews: some View {
+        LetterBox()
+    }
+}
