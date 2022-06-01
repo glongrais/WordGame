cfa33d0da41255d79d5e3da23e4b4c5396bceec1
Guillaume Longrais <guillaume.longrais@orange.fr>
2022-06-01 17:17:04 +0200

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by Guillaume Longrais on 2022-06-01.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
